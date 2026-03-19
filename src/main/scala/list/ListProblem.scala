fd39fcf7f8199975e5432a95cddc20e895a5e8e1
Mahesh Chand <mahesh.kndpl@gmail.com>
2026-03-19 13:57:30 +0530

    Started with List

---
+++ src/main/scala/list/ListProblem.scala
@@ -0,0 +1,40 @@
+package list
+
+import scala.annotation.tailrec
+
+sealed abstract class RList[+T] {
+  def head:T
+  def tail: RList[T]
+  def isEmpty: Boolean
+  //Changed prepend name to :: to make it right associative
+  // RNil.::(2) == 2 :: RNil
+  def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
+}
+
+case object RNil extends RList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: RList[Nothing] = throw new NoSuchElementException
+  override def isEmpty: Boolean = true
+  override def toString: String = "[]"
+}
+
+//Renamed Cons to :: as scala original collection
+case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
+  override def isEmpty: Boolean = false
+
+  override def toString: String = {
+    @tailrec
+     def toStringTailRec(remaining: RList[T], result: String): String = {
+       if(remaining.isEmpty) result
+       else if(remaining.tail.isEmpty) s"$result${remaining.head}"
+       else toStringTailRec(remaining.tail, s"$result${remaining.head}, ")
+     }
+    "["  + toStringTailRec(this, "") + "]"
+  }
+
+}
+
+object ListProblem extends App {
+  val aSmallList =  1 :: 2 :: 3 :: RNil // RNil.::(3).::(2).::(1)
+  println(aSmallList)
+}
