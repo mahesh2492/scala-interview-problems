5665a3e6accebc184356950cb449ec40830c4530
Mahesh Chand <mahesh.kndpl@gmail.com>
2026-04-14 06:54:24 +0530

    Generate Parentheses

--- src/main/scala/strings/ParenthesisProblem.scala
+++ src/main/scala/strings/ParenthesisProblem.scala
@@ -23,6 +23,38 @@ object ParenthesisProblem extends App {
     validParenTailRec(string, 0)
   }
 
+  /*
+     n = 1 => List("()")
+     n = 2 => List("()()", "(())")
+     n = 3 => List("()()()","()(())", "(())()" ,"((()))")
+   */
+  def generateAllValidParentheses(n: Int): List[String] = {
+     @tailrec
+     def genParensTailrec(nRemainingParens: Int, currentString: Set[String]): Set[String] = {
+       if(nRemainingParens == 0) currentString
+       else {
+         val newStrings = for {
+           string <- currentString
+           index <- 0 until string.length
+         } yield {
+           val (before, after) = string.splitAt(index)
+           s"$before()$after"
+         }
+         genParensTailrec(nRemainingParens - 1, newStrings)
+       }
+     }
+    assert(n >= 0)
+    if(n == 0) List()
+    else genParensTailrec(n, Set("()")).toList
+
+  }
+
+  def testGenParens() = {
+    println(generateAllValidParentheses(1))
+    println(generateAllValidParentheses(2))
+    println(generateAllValidParentheses(3))
+    println(generateAllValidParentheses(10))
+  }
   def testValidParenthenses() = {
     println(hasValidParenthesis("()"))
     println(hasValidParenthesis(")("))
@@ -32,5 +64,8 @@ object ParenthesisProblem extends App {
     println(hasValidParenthesis(")()"))
   }
 
-  testValidParenthenses()
+  //testValidParenthenses()
+
+  testGenParens()
+
 }
